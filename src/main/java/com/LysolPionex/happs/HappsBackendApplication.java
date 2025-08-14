a70e85468b3d5edd20386aeb43e403fdbc9d2cc7
LysolPionex <11550008+LysolPionex@users.noreply.github.com>
2025-08-14 01:55:23 -0500

    Initial project setup

---
+++ src/main/java/com/LysolPionex/happs/HappsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.LysolPionex.happs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LysolPionex/happs/HappsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LysolPionex.happs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
